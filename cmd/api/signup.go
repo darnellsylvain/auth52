23581285bdfadd5a20ad4b417a4ecbf9ae5d943a
Darnell <darnellsylvain@googlemail.com>
2025-03-23 22:23:53 +0000

    Added some comments

--- cmd/api/signup.go
+++ cmd/api/signup.go
@@ -1,6 +1,10 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/darnellsylvain/auth52/models"
+)
 
 type SignupParams struct {
 	Email 		string `json:"email"`
@@ -8,9 +12,29 @@ type SignupParams struct {
 }
 
 func (api *API) Signup(w http.ResponseWriter, r *http.Request) {
+	// Get params passed in
 	params := &SignupParams{}
-
+	// read params/decode
 	readJSON(w, r, params)
-	sendJSON(w, http.StatusOK, params)
+	
+	// Do validation checks on the params passed in. Makesure email and password in right format
+
+	// Do a DB query to find the email, if it exists send an error
+
+	// If a user doesn't exist already, create a new user and store in DB. This can be own function
+		// Create new model instance
+	user, err := models.NewUser(params.Email, params.Password)
+	if err != nil {
+		// handle error
+		sendJSON(w, http.StatusInternalServerError, nil)
+	}
+		// Set any fields you need
+	user.Provider = "email"
+		// Store in DB
+		// Handle any errors
+
+
+	// Send a response back with HTTP Ok and user
+	sendJSON(w, http.StatusOK, user)
 
 }
