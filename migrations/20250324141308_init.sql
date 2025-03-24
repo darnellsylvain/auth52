c918d9e21fff996db2a61a83f90246909fae9a4e
Darnell <darnellsylvain@googlemail.com>
2025-03-24 23:53:26 +0000

    created env file and migrate first file

---
+++ migrations/20250324141308_init.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users (
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    name varchar(255),
+    email citext UNIQUE NOT NULL,
+    encrypted_password bytea NOT NULL,
+    activated bool NOT NULL,
+    version integer NOT NULL DEFAULT 1
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
