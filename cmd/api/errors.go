5a241aec14e1ed18c06af9a463b84a61190f1062
Darnell <darnellsylvain@googlemail.com>
2025-04-02 16:16:25 +0100

    expanded errors

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,24 +5,46 @@ import (
 	"net/http"
 )
 
-
-
 type HTTPError struct {
-	Code				int
-	Message 			string
-	InternalError 		error
-	InternalMessage 	string
+	Code            int 				`json:"code"`
+	Message         string				`json:"message"`
+	InternalError   error				`json:"-"`
+	InternalMessage string				`json:"-"`
+	Errors			map[string]string	`json:"errors"`
 }
 
-
+func (e *HTTPError) Error() string {
+	if e.InternalMessage != "" {
+		return e.InternalMessage
+	}
+	return fmt.Sprintf("%d: %s", e.Code, e.Message)
+}
 
 func badRequestError(fmtString string, args ...interface{}) *HTTPError {
 	return httpError(http.StatusBadRequest, fmtString, args...)
 }
 
+func validationError(errors map[string]string) *HTTPError {
+		return &HTTPError{
+		Code:    http.StatusBadRequest,
+		Message: "validation failed",
+		Errors:  errors,
+	}
+}
+
+
+func unprocessableEntityError(fmtString string, args ...interface{}) *HTTPError {
+	return httpError(http.StatusUnprocessableEntity, fmtString, args...)
+}
+
+
 func httpError(code int, fmtString string, args ...interface{}) *HTTPError {
 	return &HTTPError{
-		Code: 		code,
-		Message: 	fmt.Sprintf(fmtString, args...),
+		Code:    code,
+		Message: fmt.Sprintf(fmtString, args...),
 	}
+}
+
+func handleError(err *HTTPError, w http.ResponseWriter) {
+	sendJSON(w, err.Code, err)
 }
