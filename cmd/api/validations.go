84bbb122d900e6e98d6d9452858b08a669b7f530
Darnell <darnellsylvain@googlemail.com>
2025-04-03 16:22:42 +0100

    disable validations

--- cmd/api/validations.go
+++ cmd/api/validations.go
@@ -1,9 +1,6 @@
 package api
 
 import (
-	"strings"
-	"unicode"
-
 	validator "github.com/darnellsylvain/auth52/internal"
 )
 
@@ -18,10 +15,10 @@ func ValidatePassword(v *validator.Validator, password string) {
 	v.Check(len(password) >= 8, "password", "must be at least 8 characters long")
 	v.Check(len(password) <= 72, "password", "must not be more than 72 characters long")
 
-	v.Check(strings.IndexFunc(password, unicode.IsUpper) >= 0, "password", "must contain an uppercase letter")
-	v.Check(strings.IndexFunc(password, unicode.IsLower) >= 0, "password", "must contain a lowercase letter")
-	v.Check(strings.IndexFunc(password, unicode.IsDigit) >= 0, "password", "must contain a digit")
-	v.Check(strings.ContainsAny(password, "!@#$%^&*"), "password", "must contain a special character (!@#$%^&*)")
+	// v.Check(strings.IndexFunc(password, unicode.IsUpper) >= 0, "password", "must contain an uppercase letter")
+	// v.Check(strings.IndexFunc(password, unicode.IsLower) >= 0, "password", "must contain a lowercase letter")
+	// v.Check(strings.IndexFunc(password, unicode.IsDigit) >= 0, "password", "must contain a digit")
+	// v.Check(strings.ContainsAny(password, "!@#$%^&*"), "password", "must contain a special character (!@#$%^&*)")
 }
 
 func ValidateSignupParams(v *validator.Validator, params *SignupParams) {
