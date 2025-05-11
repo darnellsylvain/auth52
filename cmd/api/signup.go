b9d7cc3be0cd085ac5dcd56165f0192f9c2b4cc7
Darnell <darnellsylvain@googlemail.com>
2025-05-11 23:40:56 +0100

    simplified signup

--- cmd/api/signup.go
+++ cmd/api/signup.go
@@ -6,15 +6,12 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/darnellsylvain/auth52/internal/database"
 	validator "github.com/darnellsylvain/auth52/internal/validator"
+	"github.com/jackc/pgx/v5/pgconn"
 
 	"github.com/darnellsylvain/auth52/models"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgtype"
 )
-var query = `INSERT INTO users (name, email, encrypted_password, activated, provider) 
-		VALUES ($1, $2, $3, $4, $5)
-		RETURNING id, created_at, version`
 
 var ErrDuplicateEmail = errors.New("email already in use")
 
@@ -41,50 +38,38 @@ func (api *API) Signup(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
 	defer cancel()
 
-	tx, err := api.db.Begin(ctx)
-	if err != nil {
-		api.serverErrorResponse(w, r, err)
-		return
-	}
-	defer tx.Rollback(ctx)
 
-	user, err := signupNewUser(ctx, tx, *params)
+	user, err := signupNewUser(ctx, api.queries, *params)
 	if err != nil {
 		api.badRequestError(w, r, err)
-		return
-	}
-
-	if err := tx.Commit(ctx); err != nil {
-    	api.serverErrorResponse(w, r, err)
-    	return
+		return 
 	}
 
 	sendJSON(w, http.StatusOK, user, nil)
 }
 
-func signupNewUser(ctx context.Context, tx pgx.Tx, params SignupParams) (*models.User, error) {
-	var id pgtype.UUID
-	err := tx.QueryRow(ctx, `SELECT id FROM users WHERE email = $1`, params.Email).Scan(&id)
-	if err == nil {
-		return nil, ErrDuplicateEmail
-	} else  {
-    	switch err {
-    		case pgx.ErrNoRows:
-        	break
-    		default:
-        		return nil, err
-    	}
-	}
+func signupNewUser(ctx context.Context, queries *database.Queries, params SignupParams) (*models.User, error) {
+    user, err := models.NewUser(params.Email, params.Password)
+    if err != nil {
+        return nil, err
+    }
 
-	user, err := models.NewUser(params.Email, params.Password)
-	if err != nil {
-		return nil, err
-	}
+    createdUser, err := queries.CreateUser(ctx, database.CreateUserParams{
+        Name:              user.Name,
+        Email:             user.Email,
+        EncryptedPassword: user.EncryptedPassword,
+        Activated:         user.Activated,
+        Provider:          user.Provider,
+    })
+    if err != nil {
+        var pgErr *pgconn.PgError
+        if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+            return nil, ErrDuplicateEmail
+        }
+        return nil, err
+    }
 
-	_, err = tx.Exec(ctx, query, user.Name, user.Email, user.EncryptedPassword, user.Activated, user.Provider)
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
+    user.ID = createdUser.ID
+    user.CreatedAt = createdUser.CreatedAt.Time
+    return user, nil
 }
